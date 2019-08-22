42d8e5177e12b3de672e49e0e5020def8927d070
js103 <643929860@qq.com>
2019-08-22 09:51:59 +0700

    异常类的处理(定位，封装）

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,8 +1,17 @@
 const Router = require("koa-router");
 const router = new Router();
 
-router.get('/v1/classic', (ctx, next) => {
+router.post('/v1/:id/classic', (ctx, next) => {
+
+    const path = ctx.params
+    const query = ctx.request.query
+    const headers = ctx.request.header
+    const body = ctx.request.body
+    console.log(path,query,headers,body,)
     ctx.body = { key: "classic" };
+
+    throw new Error('API Exception')
+
 })
 
 
--- core/init.js
+++ core/init.js
@@ -2,29 +2,32 @@ const requireDirectory = require("require-directory");
 const Router = require("koa-router");
 
 
-class InitManager{
-    static initCore(app) {
-        //入口方法
-        InitManager.app = app
+class InitManager {
+	static initCore(app) {
+		//入口方法
+		InitManager.app = app;
         InitManager.initLoadRouters();
+        InitManager.loadConfig();
+	}
 
+	static loadConfig(path = "") {
+		const configPath = path || process.cwd() + "/config/config.js";
+		const config = require(configPath);
+		global.config = config;
+	}
 
-    }
-
-    static initLoadRouters() {
-        //path config
-        const apiDirectory = `${process.cwd()}/app/api`;
-        requireDirectory(module, apiDirectory, {
-            visit: whenLoadModule
-        });
-        function whenLoadModule(obj) {
-            if (obj instanceof Router) {
-                InitManager.app.use(obj.routes());
-            }
-        }
-
-
-    }
+	static initLoadRouters() {
+		//path config
+		const apiDirectory = `${process.cwd()}/app/api`;
+		requireDirectory(module, apiDirectory, {
+			visit: whenLoadModule
+		});
+		function whenLoadModule(obj) {
+			if (obj instanceof Router) {
+				InitManager.app.use(obj.routes());
+			}
+		}
+	}
 }
 
 
